29ecdfb71d6bab45ff5012f6ccde5efba1af0ae2
Emishimi <146140603+Emishimi@users.noreply.github.com>
2024-01-04 22:02:34 +0100

    First commit wasm attempt

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,9 @@
 mod handlers;
-//mod models;
-//mod utils;
-//mod config;
+// mod models;
+// mod utils;
+// mod config;
 
+use actix_files as fs;
 use actix_web::{App, HttpServer, web};
 use crate::handlers::*;
 
@@ -10,6 +11,8 @@ use crate::handlers::*;
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .service(fs::Files::new("/pkg", "./wasm_chat_client/pkg").show_files_listing())
+            .service(fs::Files::new("/", "./static").index_file("index.html"))
             .route("/", web::get().to(handlers::greet))
             .route("/ws/", web::get().to(handlers::chat_route))
     })
